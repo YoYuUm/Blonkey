f9c2e9f1741eb2a23d51d0fe901ea3a1d49c4cc1
Rulo <serulo@gmail.com>
2013-09-23 17:12:50 +0300

    Heroku update

---
+++ Procfile
@@ -0,0 +1,2 @@
+web: gunicorn run_heroku:app
+init: python createdb.py & python createadmin.py 
---
+++ createadmin.py
@@ -0,0 +1,10 @@
+
+from app import db
+from app.models import User
+user = User()
+user.nickname = "admin"
+user.email = "admin@admin.com"
+user.password = "FastMonkeys"
+user.role = 1;
+db.session.add(user)
+db.session.commit()
