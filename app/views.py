8a8dca6bf9f8ab004ff5a8cd6ff5ea8a04c36d6b
Rulo <serulo@gmail.com>
2013-09-26 11:48:45 +0300

    Templating and adding 404 and 500

--- app/views.py
+++ app/views.py
@@ -227,4 +227,12 @@ def logout():
     flash('Successfully logged out')
     return redirect(url_for('index'))
 
+@app.errorhandler(404)
+def page_not_found(error):
+    return render_template('404.html'), 404
+
+@app.errorhandler(500)
+def internal_error(error):
+    return render_template('500.html'), 500
+
 #TODO tests
--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 import os
 
 if os.environ.get('DATABASE_URL') is None:
-    SQLALCHEMY_DATABASE_URI = "postgres://jbqwkgvkchdwtd:8EYMko4APC4UgzdbjbaGMYgPfU@ec2-184-73-162-34.compute-1.amazonaws.com:5432/d6bp5f6dd0vmv2" #backup
+    SQLALCHEMY_DATABASE_URI = "postgres://poekatqwjbhijk:TpcAx4OlFpzF9KwsP-B69fJjQG@ec2-54-228-224-127.eu-west-1.compute.amazonaws.com:5432/dejvj3b1artnp7" #backup
 else:
     SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
 
